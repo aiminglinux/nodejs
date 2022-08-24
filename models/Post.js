5ba3bfb3ef7a67fdfd0b82e18c7c1283367d04bb
fReeman <reg@officience.com>
2022-08-24 15:56:31 +0700

    Add Post model

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Scheme = mongoose.Schema;
+
+const PostScheme = new Scheme({
+  user: {
+    type: Scheme.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Scheme.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Scheme.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostScheme);
